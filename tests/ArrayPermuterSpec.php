55fbca09a4a8b9db4833af2e16cea31655090654
Jon Håvard Gundersen <jhgundersen@gmail.com>
2015-05-23 14:13:43 +0200

    Added tests for callback to object

--- tests/ArrayPermuterSpec.php
+++ tests/ArrayPermuterSpec.php
@@ -50,6 +50,25 @@ class ArrayPermuterSpec extends \PHPUnit_Framework_TestCase {
 	/**
 	 * @test
 	 */
+	public function should_be_able_to_use_callback_to_method_on_an_object_for_each_permutation(){
+		// given
+		$variations = [];
+		$array = new ArrayPermuter([1,2]);
+		$handler = new PermutationHandler();
+	
+		// when
+		$array->walk(array($handler, 'handle'));
+
+		// then
+		$this->assertEquals([
+			[1, 2], 
+			[2, 1]
+		], $handler->variations);
+	}
+
+	/**
+	 * @test
+	 */
 	public function should_ignore_keys_when_walking_an_associative_array(){
 		// given
 		$variations = [];
@@ -65,5 +84,13 @@ class ArrayPermuterSpec extends \PHPUnit_Framework_TestCase {
 			[1, 2], 
 			[2, 1]
 		], $variations);
+	}
+}
+
+class PermutationHandler {
+	public $variations = [];
+
+	public function handle($variation){
+		$this->variations[] = $variation;
 	}
 }
